b25d06d6916b76a41c164f5d4f375ee72ad549b0
samohung390 <samohung390@gmail.com>
2017-12-26 14:49:45 +0800

    gngngngnng

--- WebApplication8/Startup.cs
+++ WebApplication8/Startup.cs
@@ -17,7 +17,7 @@ namespace WebApplication8
         }
 
         public IConfiguration Configuration { get; }
-        //ksdfklsdklfdklsflsdfkls
+        //ksdfklsdklfdklsflsdfklsqweqweqwdwfdw
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
