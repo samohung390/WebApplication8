931713e5b1bac5f101be3bb3da00d3a9408aacd1
samohung390 <samohung390@gmail.com>
2017-12-26 14:20:50 +0800

    rgfbrgvrvre

--- WebApplication8/Startup.cs
+++ WebApplication8/Startup.cs
@@ -23,7 +23,7 @@ namespace WebApplication8
         {
             services.AddMvc();
         }
-        //asdadadadadadad
+        //asdadadadadadadtg4tgrtgr4grtghrt
         //dfsvnkjsnfsnfklmsl
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
